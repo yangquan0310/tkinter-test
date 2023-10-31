c7499a17dea68e489eac6054f7c53b4982f40052
yangquan0310 <yangquan0310@163.com>
2023-10-31 16:26:57 +0800

    天干地支

--- App/components/Input.py
+++ App/components/Input.py
@@ -1,5 +1,6 @@
 from tkinter import *
 from tkinter.messagebox import *
+from borax.calendars.lunardate import LunarDate
 class InputFrame(Frame): # 继承Frame类 
     def __init__(self, master=None): 
         Frame.__init__(self, master) 
